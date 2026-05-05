607b5dc52ef7528f47423a78d68ecdcd16c3a6d1
zishanx <zishanalikhan53@gmail.com>
2026-05-05 22:49:37 +0530

    Saas template done

--- src/components/template/SaaSTemplate.jsx
+++ src/components/template/SaaSTemplate.jsx
@@ -30,23 +30,70 @@ export default function SaaSTemplate({ content }) {
             <div id="features" className="px-30 py-10">
                 <h2 className="font-bold text-3xl text-center">Features</h2>
                 <div id="cards " className="p-10 flex flex-row gap-6">
-                        <div id="card" className="p-4 flex flex-col text-center items-center justify-center bg-black-400">
-                            <span>⚡</span>
-                            <h3>{content.feature1Title}</h3>
-                            <p>{content.feature1Desc}</p>
+                        <div id="card" className="p-4 flex flex-col text-center items-center justify-between bg-black/50 p-10 gap-4 rounded">
+                            <span className="text-2xl">⚡</span>
+                            <h3 className="font-bold text-2xl text-purple-400">{content.feature1Title}</h3>
+                            <p className="text-gray-400">{content.feature1Desc}</p>
                         </div>
-                        <div id="card" className="p-4 flex flex-col text-center items-center justify-center">
-                            <span>⚡</span>
-                            <h3>{content.feature1Title}</h3>
-                            <p>{content.feature1Desc}</p>
+
+                        <div id="card" className="p-4 flex flex-col text-center items-center justify-between bg-black/50 p-10 gap-4 rounded">
+                            <span className="text-2xl">🤖</span>
+                            <h3 className="font-bold text-2xl text-purple-400">{content.feature2Title}</h3>
+                            <p className="text-gray-400">{content.feature2Desc}</p>
                         </div>
-                        <div id="card" className="p-4 flex flex-col text-center items-center justify-center">
-                            <span>⚡</span>
-                            <h3>{content.feature1Title}</h3>
-                            <p>{content.feature1Desc}</p>
+
+                        <div id="card" className="p-4 flex flex-col text-center items-center justify-between bg-black/50 p-10 gap-4 rounded">
+                            <span className="text-2xl">🔒</span>
+                            <h3 className="font-bold text-2xl text-purple-400">{content.feature3Title}</h3>
+                            <p className="text-gray-400">{content.feature3Desc}</p>
                         </div>
+                        
                 </div>
             </div>
+
+
+            <section id="stats" className="bg-black/30 px-30 py-10  shadow">
+                    <div className="flex   justify-center gap-10 w-full">
+                        <div className="flex-1 text-center flex
+                        flex-col gap-3">
+                            <span className="font-bold text-5xl">{content.stat1Number}</span>
+                            <p className="text-gray-400">{content.stat1Label}</p>
+                        </div>
+                        
+                        <div className="flex-1 text-center flex
+                        flex-col gap-3">
+                            <span className="font-bold text-5xl">{content.stat2Number}</span>
+                            <p className="text-gray-400">{content.stat2Label}</p>
+                        </div>
+
+                        <div className="flex-1 text-center flex
+                        flex-col gap-3">
+                            <span className="font-bold text-5xl">{content.stat3Number}</span>
+                            <p className="text-gray-400">{content.stat3Label}</p>
+                        </div>
+                    </div>
+            </section>
+
+            <section id="testimonials" className="px-30 py-20">
+                <div className="max-w-2xl mx-auto bg-black/40 border-l-4 border-purple-500 p-8 rounded">
+                <p className="text-center my-2 font-bold italic">{`"${content.testimonialQuote}"`}</p>
+                <p className="text-center">- {content.testimonialName}, <span className="text-gray-400">{content.testimonialRole}</span></p>
+                </div>
+            </section>
+
+            <section id="CTA" className="bg-purple-700 px-30 py-20">
+
+                <div className=" text-center flex flex-col gap-6  items-center">
+                        <h2 className="text-4xl font-bold">{content.ctaBannerHeading}</h2>
+                        <p>{content.ctaBannerSub}</p>
+                        <button className="bg-white text-black px-6 py-4 rounded w-fit mx-auto">{content.ctaBannerBtn}</button>
+                </div>
+
+            </section>
+
+            <footer id="footer " className="bg-gray-950 p-8 text-center text-gray-400">
+                <p className="">{content.footer}</p>
+            </footer>
         </>
     )
 }
